7aa2391f5bfd0d8c11db5448f7a7be3c79c527aa
Simone-Corso <scorso26@gmail.com>
2023-11-13 16:30:05 +0100

    Ho inserito button element con query selector'button'

--- js/idenx.js
+++ js/idenx.js
@@ -10,3 +10,17 @@
 
 //Quanti km vuoi fare e quanti anni hai?
 
+/*
+
+STEPS:
+
+1. eseguire del codice solo quando l'utente clicca - console.log() e creazione di un evento con addEventListener()
+2.andare a prendere il valore che l'utente ha inserito negli input - getElementById e .value
+3. i valori inseriti dall'utente negli input devono essere salvati in delle variabili
+
+*/
+
+const buttonElement = document.querySelector('button');
+
+let km = document.getElementById('km').value;
+console.log('km: ' + km);
